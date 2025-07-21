745a4fd2c77c483c40714fd9981a96ffb2fd5d8a
Enzo Patti <enzo.patti@aluno.senai.br>
2025-07-21 11:26:09 -0300

    navbar added to layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,33 @@
 import type { Metadata } from "next";
-import {Inter } from "next/font/google";
+import { Inter } from "next/font/google";
+import { Navbar } from "@/components/layout/Navbar";
 import "./globals.css";
 
-
 const geistSans = Inter({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
+ subsets: ["latin"],
+ variable: "--font-geist-sans",
 });
 const geistMono = Inter({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
-  display: "swap",
+ subsets: ["latin"],
+ variable: "--font-geist-mono",
+ display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "Mottu | Bonificação",
-  description: "Reconhecendo quem faz a diferença!",
+ title: "Mottu | Bonificação",
+ description: "Reconhecendo quem faz a diferença!",
 };
 export default function RootLayout({
-  children,
+ children,
 }: Readonly<{
-  children: React.ReactNode;
+ children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+ return (
+  <html lang="pt-BR">
+   <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <Navbar />
+    <main>{children}</main>
+   </body>
+  </html>
+ );
 }
